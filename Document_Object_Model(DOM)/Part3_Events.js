0523d5734404a9c189dd107c9240f48f424dd0cb
Allen <allenwem@gmail.com>
2019-04-20 23:18:00 -0700

    DOM finished

--- Document_Object_Model(DOM)/Part3_Events.js
+++ Document_Object_Model(DOM)/Part3_Events.js
@@ -1,29 +1,30 @@
 //Let's explore some events samples!
 
-var headOne = document.querySelector('#one')
-var headTwo = document.querySelector('#two')
-var headThree = document.querySelector('#three')
+var headOne = document.querySelector("#one")
+var headTwo = document.querySelector("#two")
+var headThree = document.querySelector("#three")
 
 // Hover (mouseover and mouseout)
-headOne.addEventListener('mouseover',function(){
-  headOne.textContent = "Mouse currently Over";
+headOne.addEventListener('mouseover', function()
+{
+  headOne.textContent = "Mouse Currently Over";
   headOne.style.color = 'red';
 })
 
-headOne.addEventListener('mouseout',function(){
+headOne.addEventListener('mouseout', function(){
   headOne.textContent = "Mouse Not On me."
   headOne.style.color = 'blue';
 })
 
 
 // On Click
-headTwo.addEventListener("click",function(){
+headTwo.addEventListener("click", function(){
   headTwo.textContent = "Clicked On";
   headTwo.style.color = 'blue';
 })
 
 // Double Click
-headThree.addEventListener("dblclick",function(){
+headThree.addEventListener("dblclick", function(){
   headThree.textContent = "Double Clicked!";
   headThree.style.color = 'red';
 })
