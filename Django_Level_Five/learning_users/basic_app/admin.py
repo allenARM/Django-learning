8031ffb11dd0bea21c380ea6709219a8681f2cef
Allen Melikian <allenwem@gmail.com>
2019-04-26 16:21:38 -0700

    finished django learning, next practice

---
+++ Django_Level_Five/learning_users/basic_app/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from basic_app.models import UserProfileInfo
+
+# Register your models here.
+
+admin.site.register(UserProfileInfo)
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 # Django-learning
 In this is repository I am learning Django and web development
 
-Last watched lesson 20-146
+Last watched lesson 21-154
