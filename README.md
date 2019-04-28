88691537c5a6a58f132ab092db4bfffad94d2d8b
Allen Melikian <allenwem@gmail.com>
2019-04-27 17:27:17 -0700

    Django advanced finished

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 # Django-learning
 In this is repository I am learning Django and web development
 
-Last watched lesson 22-161
+Last watched lesson 23-163
--- advanced_section/advcbv/basic_app/views.py
+++ advanced_section/advcbv/basic_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import View, TemplateView, ListView, DetailView
+from django.urls import reverse, reverse_lazy
+from django.views.generic import (View, TemplateView,
+									ListView, DetailView,
+									CreateView, UpdateView,
+									DeleteView)
 from . import models
 
 class IndexView(TemplateView):
@@ -15,3 +19,15 @@ class SchoolDetailView(DetailView):
 	context_object_name = 'school_detail'
 	model = models.School
 	template_name = 'basic_app/schooldetail.html'
+
+class SchoolCreateView(CreateView):
+	fields = ('name', 'principal', 'location')
+	model = models.School
+
+class SchoolUpdateView(UpdateView):
+	fields = ('name', 'principal')
+	model = models.School
+
+class SchoolDeleteView(DeleteView):
+	model = models.School
+	success_url = reverse_lazy("basic_app:list")
